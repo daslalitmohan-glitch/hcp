744ec26c164848908f47657b3dc97a46ee620501
daslalitmohan-glitch <daslalitmohan@gmail.com>
2025-10-09 09:02:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  count         = 1
+  count         = 2
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
   tags = {
-    Name = "Mohan"
+    Name = "das"
   }
 }
