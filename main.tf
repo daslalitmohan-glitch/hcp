2ce60ef0e8cf5a06157db21a4dce0669d6072c52
Lalit Das <daslalitmohan@gmail.com>
2025-10-09 21:33:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  count         = 2
+  count         = 1
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
   tags = {
-    Name = "Das"
+    Name = "LMD"
   }
 }
